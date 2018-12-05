25c904c208863eebef290171b004c6bc61a5d1eb
systems <systems@DESKTOP-GG8VC3E>
2018-12-05 16:00:34 +0530

    pcf with mysql

---
+++ restwebservice/src/main/java/com/packt/model/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.packt.model;
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+    
+     void deleteByFirstName(String firstName);
+}
